88361a1c58f0957ea7a02cfcbf80c0c641ef0f45
Claudio Sampaio <claudio.sampaio@conatussw.com>
2017-02-11 13:47:14 -0400

    Destruindo as balas quando atigem objeto. Fixando a camera no eixo Y.

---
+++ exemplos/ChoppChoppChopp/Assets/Scripts/FixCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCamera : MonoBehaviour {
+    private Vector3 camPos;
+    private Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+        mainCamera = GetComponent<Camera>();
+        camPos = mainCamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 newPos = new Vector3(mainCamera.transform.position.x, camPos.y, camPos.z);
+        mainCamera.transform.position = newPos;
+	}
+}
