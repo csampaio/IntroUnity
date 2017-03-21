bd8badebe68f4fa079221fdcebc023f7d8710e93
Claudio Sampaio <claudio.sampaio@conatussw.com>
2017-03-21 13:52:59 -0400

    Melhoria na criação das pessoas e dos prédios.

--- exemplos/ChoppChoppChoopV2/Assets/Scripts/ObjectsCreator.cs
+++ exemplos/ChoppChoppChoopV2/Assets/Scripts/ObjectsCreator.cs
@@ -9,22 +9,36 @@ public class ObjectsCreator : MonoBehaviour {
 	public GameObject[] Enemies;
 	public Transform spawnPoint;
 
+    private PlayerController player;
+
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(SpawnObject(peoples[0], 2f));
-		StartCoroutine(SpawnObject(builds[0], 2.5f));
-		StartCoroutine(SpawnObject(builds[1], 3f));
-		StartCoroutine(SpawnObject(builds[2], 4f));
+        player = FindObjectOfType<PlayerController>();
+		StartCoroutine(SpawnObject(peoples[0], 2f, 5f));
+        StartCoroutine(SpawnBuilds(3f,7f));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private IEnumerator SpawnObject(GameObject obj, float minDelay, float maxDelay) {
+        if (player.horizontalSpeed > 0)
+        {
+            Instantiate(obj, spawnPoint.position, Quaternion.identity, this.transform);
+        }
+        float delay = Random.Range(minDelay, maxDelay);
+        yield return new WaitForSeconds (delay);
+		StartCoroutine (SpawnObject(obj, maxDelay, minDelay));
 	}
 
-	private IEnumerator SpawnObject(GameObject obj, float delay) {
-		Instantiate (obj, spawnPoint.position, Quaternion.identity, this.transform);
-		yield return new WaitForSeconds (delay);
-		StartCoroutine (SpawnObject(obj, delay));
-	}
+    private IEnumerator SpawnBuilds(float minDelay, float maxDelay)
+    {
+        int i = Random.Range(0, builds.Length);
+        GameObject build = builds[i];
+        if (player.horizontalSpeed > 0)
+        {
+            Instantiate(build, spawnPoint.position, Quaternion.identity, this.transform);
+        }
+        float delay = Random.Range(minDelay, maxDelay);
+        yield return new WaitForSeconds(delay);
+        StartCoroutine(SpawnBuilds(maxDelay, minDelay));
+    }
 }
